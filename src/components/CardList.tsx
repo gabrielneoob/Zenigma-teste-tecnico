d0f465e73bb5a7e3cfb7be1390bddaed08cf3e62
Gabriel Neoob <gabrielngc@gmail.com>
2023-08-08 16:29:38 -0300

    CardList solution

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { useState } from 'react'
+import { Styles } from './Styles';
+type CardListProps = {
+  size: number;
+}
 
-const CardList = () => {
+const CardList = ({ size }:CardListProps ) => {
+  const [findIndex, setFindIndex] = useState<number>(-1)
+  const arr = Array.from({length: size}, () => 0);
+
+  const handleRevealCard = (index: number) => {
+    setFindIndex(index)
+  }
+
+
+  
   return (
-    <div>CardList</div>
+    <div>
+      <Styles.CardContainer>
+        {arr.map((_, index) => {
+          return (
+            <Styles.Card
+            test={findIndex === index}
+            key={index}
+            onClick={() => handleRevealCard(index)}
+            role='menuitem'
+            >
+              <Styles.Title
+              test={findIndex === index}
+              >
+                {findIndex === index ? 'up' : 'down'}
+              </Styles.Title>
+            </Styles.Card>
+          )
+        })}
+      </Styles.CardContainer>
+    </div>
   )
 }
 
